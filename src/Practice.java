6d1a95070492f0bc0c08a448dd8ab552268f0206
Elvin Hrytsyuk <12345678+username@users.noreply.github.com>
2025-11-01 16:50:27 -0700

    Finshed the maxDiff method and added a test for all negative

--- src/Practice.java
+++ src/Practice.java
@@ -7,13 +7,27 @@ public class Practice {
      */
     public static int maxDiff(int[] nums) {
         // TODO: implement this
-        return -1;
-    }
+        // variables to hold the largest and smallest
+        int largest = nums[0];
+        int smallest = nums[0];
 
+        // go through the array to find largest and smallest
+        for (int num : nums) {
+            // comapre to current largest and smallest
+            if (num > largest) {
+                largest = num;
+            } else if (num < smallest) {
+                smallest = num;
+            }
+        }
+        // we return the difference
+        return largest - smallest;
+    }
 
     // TODO: Implement the other methods from the study guide AND tests for each one
 
     // For each method you are only required to implement it for one of the data
     // structures. But use a different data structure for each method. For example,
     // do maxDiff with an array, the next question with a Set, etc.
+
 }
--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+import java.util.*;
+// forgot needed to download the red hat
 
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +10,7 @@ public class PracticeTest {
     @Test
     void testMaxDiffPositiveAndNegative() {
         // Arrange
-        int[] numbers = {8, -2, 9, -5};
+        int[] numbers = { 8, -2, 9, -5 };
 
         // Act
         int actual = Practice.maxDiff(numbers);
@@ -17,10 +19,20 @@ public class PracticeTest {
         // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
         assertEquals(14, actual);
     }
-    
+
+    @Test
+    void testMaxDiffAllNegative() {
+        // Arrange
+        int[] numbers = { -4, -5, -1, -3 };
+
+        // Act
+        int actual = Practice.maxDiff(numbers);
+
+        // Assert
+        // largest: -1, smallest: -5, difference: -1 -5 = 4
+        assertEquals(4, actual);
+    }
 
     // TODO: Make tests for each problem you solve
-    
+
 }
-
-
